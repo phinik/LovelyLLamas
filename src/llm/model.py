6cf83c138ae060c685b7c7b23f773737927c0a54
Philipp Donn <30521025+phinik@users.noreply.github.com>
2024-10-27 12:08:37 +0100

    added stubs for transormer and lstm models

---
+++ src/llm/model.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+class Transformer(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x): 
+        pass
+
+    def save_to(self, path: str):
+        pass
+
+    def load_from(self, path: str):
+        pass
+
+
+class LSTM(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+    def save_to(self, path: str):
+        pass
+
+    def load_from(self, path: str):
+        pass
