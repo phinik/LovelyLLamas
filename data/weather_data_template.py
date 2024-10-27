96458b8f1bc44be90b0482b9b33babb325d2d942
Philipp Donn <30521025+phinik@users.noreply.github.com>
2024-10-27 16:36:16 +0100

    weather data template

---
+++ data/weather_data_template.py
@@ -0,0 +1,24 @@
+{
+    "city": "Hamburg",
+    "created_day": "2024-10-26",
+    "created_time": "21:00:33",
+    "report_short": "In Hamburg ist es morgens grau und es bleibt neblig und die Temperatur liegt bei 7°C. Am Mittag wechseln sich Wolken und Sonne ab bei Höchstwerten von 16°C. Abends ist es in Hamburg locker bewölkt und die Temperaturen liegen zwischen 12 und 14 Grad. Nachts gibt es nur selten Lücken in der Wolkendecke bei Tiefstwerten von 11°C.  Böen können Geschwindigkeiten zwischen 19 und 22 km/h erreichen.",
+    "report_long": "Wetter heute, 26.10.2024 In Hamburg wird am Morgen die Sicht durch Nebel eingeschränkt und die Temperatur liegt bei 7°C. Mittags zeigt sich die Sonne nur vereinzelt bei sonst wolkigem Himmel und die Temperatur steigt auf 16°C. Am Abend bilden sich in Hamburg vereinzelt Wolken bei Temperaturen von 12 bis 14°C. Nachts ist ein Blick auf die Sterne nur vereinzelt bei sonst wolkigem Himmel möglich bei Tiefstwerten von 11°C. Mit Böen zwischen 19 und 22 km/h ist zu rechnen. Gefühlt liegen die Temperaturen bei 7 bis 17°C. Hamburg liegt in den Regionen Lüneburger Heide und Nordsee . Öffnen Sie eine Region um eine Wettervorhersage für die gesamte Region zu erhalten.",
+    "sunrise": "08:08 Uhr",
+    "sundown": "17:58 Uhr",
+    "sunhours": "2 h",
+
+    # aus "overview"
+    "times": ["20-21", "21-22", "22-23", "23-00", "01-02", "02-03", "03-04", "04-05", "05-06", "06-07", "07-08", "08-09", "09-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16", "16-17", "17-18", "18-19", "19-20"],
+    "clearness": ["Wolkig", "Leicht bewölkt", "Leicht bewölkt", "Leicht bewölkt", "Wolkig", "Wolkig", "Wolkig", "Wolkig", "Bedeckt", "Bedeckt", "Wolkig", "Bedeckt", "Bedeckt", "Leichter Regen", "Wolkig", "Wolkig", "Wolkig", "Wolkig, und windig", "Wolkig", "Wolkig", "Wolkig", "Leicht bewölkt", "Klar"],
+    
+    # aus "diagram"
+    "temperatur_in_deg_C": [13,12,12,12,11,11,11,11,11,11,11,12,12,13,14,15,15,15,14,13,12,11,10],
+    "niederschlagsrisiko_in_perc": [5,25,5,0,0,0,0,0,0,0,0,15,45,90,45,5,5,20,25,20,15,25,5],
+    "niederschlagsmenge_in_l_per_sqm": [0,0,0,0,0,0,0,0,0,0,0,0,0,0.34,0,0,0,0,0,0,0,0,0],
+    "windrichtung": ["SO","SO","SO","SO","SO","SO","S","S","SW","SW","SW","SW","W","W","W","W","W","W","W","NW","NW","NW","W"],
+    "windgeschwindigkeit_in_km_per_s": [8,8,8,8,7,5,4,5,6,6,8,8,8,12,12,15,15,15,12,10,9,4,3],
+    "luftdruck_in_hpa": [1018,1017,1017,1016,1015,1015,1016,1015,1015,1015,1016,1017,1018,1018,1019,1019,1020,1020,1021,1022,1023,1023,1024],
+    "relative_feuchte_in_perc": [95,95,96,96,95,95,93,92,90,89,89,87,89,91,83,67,66,69,72,78,78,79,84],
+    "bewölkungsgrad": ["4/8","4/8","4/8","4/8","3/8","7/8","6/8","7/8","8/8","8/8","5/8","8/8","8/8","7/8","5/8","6/8","6/8","7/8","7/8","6/8","6/8","3/8","1/8"]
+}
