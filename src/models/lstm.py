df81d63b9d9be79fb5284677d585d63fc199d32a
Philipp Donn <30521025+phinik@users.noreply.github.com>
2024-11-02 11:33:43 +0100

    restructured models module

---
+++ src/models/lstm.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class LSTM(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x): 
+        pass
+
+    def save_to(self, path: str):
+        pass
+
+    def load_from(self, path: str):
+        pass
