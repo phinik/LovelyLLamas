07fbe876d8f967a2ae3b7e1883a305f2b9c29430
Philipp Donn <30521025+phinik@users.noreply.github.com>
2024-11-02 13:36:32 +0100

    fix wrong split types

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,6 +1,6 @@
 from torch.utils.data import DataLoader
 
-from dataset import WeatherDataset, Split
+from src.dataset import WeatherDataset, Split
 
 
 def get_train_dataloader_weather_dataset(path: str, batch_size: int) -> DataLoader:
@@ -9,11 +9,11 @@ def get_train_dataloader_weather_dataset(path: str, batch_size: int) -> DataLoad
     return DataLoader(dset, batch_size=batch_size, shuffle=True)
 
 def get_eval_dataloader_weather_dataset(path: str, batch_size: int) -> DataLoader:
-    dset = WeatherDataset(path=path, split=Split.TRAIN)
+    dset = WeatherDataset(path=path, split=Split.EVAL)
 
     return DataLoader(dset, batch_size=batch_size, shuffle=False)
 
 def get_test_dataloader_weather_dataset(path: str, batch_size: int) -> DataLoader:
-    dset = WeatherDataset(path=path, split=Split.TRAIN)
+    dset = WeatherDataset(path=path, split=Split.TEST)
 
     return DataLoader(dset, batch_size=batch_size, shuffle=False)
