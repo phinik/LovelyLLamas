e348ba0334c7a707b42084503f0100b06cf97db9
Philipp Donn <30521025+phinik@users.noreply.github.com>
2024-11-02 11:34:02 +0100

    added basic arguments

--- src/train.py
+++ src/train.py
@@ -1,5 +1,8 @@
 import argparse
 
+from models import Transformer
+from dataloader import *
+
 class Trainer:
     def __init__(self):
         pass
@@ -10,5 +13,13 @@ class Trainer:
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", type=str, help="")
+    parser.add_argument("--checkpoint_path", type=str, help="")
+    parser.add_argument("--tensorboard_path", type=str, help="")
+    parser.add_argument("--model", type=str, choices=["transformer", "lstm"], help="")
+    
+    args = parser.parse_args()
+    
     trainer = Trainer()
     trainer.train()
