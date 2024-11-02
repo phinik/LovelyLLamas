77c01542096aa8490654302fc0b0e356843a3a62
Philipp Donn <30521025+phinik@users.noreply.github.com>
2024-11-02 11:24:24 +0100

    impl dataloader functions

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,9 +1,19 @@
 from torch.utils.data import DataLoader
 
-from dataset import WeatherDataset
+from dataset import WeatherDataset, Split
 
-def get_train_dataloader(batch_size):
-    pass
 
-def get_eval_dataloader():
-    pass
+def get_train_dataloader_weather_dataset(path: str, batch_size: int) -> DataLoader:
+    dset = WeatherDataset(path=path, split=Split.TRAIN)
+
+    return DataLoader(dset, batch_size=batch_size, shuffle=True)
+
+def get_eval_dataloader_weather_dataset(path: str, batch_size: int) -> DataLoader:
+    dset = WeatherDataset(path=path, split=Split.TRAIN)
+
+    return DataLoader(dset, batch_size=batch_size, shuffle=False)
+
+def get_test_dataloader_weather_dataset(path: str, batch_size: int) -> DataLoader:
+    dset = WeatherDataset(path=path, split=Split.TRAIN)
+
+    return DataLoader(dset, batch_size=batch_size, shuffle=False)
