9d29e5faccde1f99bf36dd8170b8a24fefba1a54
Philipp Donn <30521025+phinik@users.noreply.github.com>
2024-11-08 10:56:52 +0100

    changed transformations to resemble torchvision transforms

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,30 +1,31 @@
-from abc import ABC, abstractmethod
 from typing import Dict
 
 # so vom stil her wie the torchvision transforms wäre glaube ich ganz cool, also für jede einzelne Operation eine
 # eigene Klasse ableiten
 
-class ITransform(ABC):
-    @abstractmethod
-    def process(self, data: Dict) -> Dict:
-        ...
-
-
-class ReplaceLineBreaksInOverview(ITransform):
+class ReplaceLineBreaksInOverview:
     def __init__(self):
         pass
 
-    def process(self, data: Dict) -> Dict:
+    def __call__(self, data: Dict) -> Dict:
         # TODO
         return data
     
 
-class IntroduceCustomTokens(ITransform):
+class IntroduceCustomTokens:
     def __init__(self):
         pass
 
-    def process(self, data: Dict) -> Dict:
+    def __call__(self, data: Dict) -> Dict:
         # TODO
         return data
 
     
+class RemoveKeys:
+    def __init__(self):
+        pass
+
+    def __call__(self, data: Dict) -> Dict:
+        # TODO
+
+        return data
