9b43365606cd356247cf7373b474067f8a386b1f
Philipp Donn <30521025+phinik@users.noreply.github.com>
2025-01-10 10:44:13 +0100

    declutter

--- src/tokenizer/set_of_words_tokenizer.py
+++ src/tokenizer/set_of_words_tokenizer.py
@@ -1,8 +1,8 @@
 import json
 import os
 
-from abc import ABC, abstractmethod
-from typing import List
+
+from typing import List, Dict
 
 
 from .itokenizer import ITokenizer
@@ -11,29 +11,22 @@ from .itokenizer import ITokenizer
 class SetOfWordsTokenizer(ITokenizer):
     _SPECIAL_TOKENS = ["<city>", "<start>", "<stop>", "<unknown>", "<padding>"]
 
-    def __init__(self, tokens_file: str, extra_tokens: List[str]):
+    def __init__(self, tokens_file: str, extra_tokens_mapping: Dict):
         super().__init__()
 
         self._tokens = self._load_tokens(tokens_file)
-
         self._tokens += self._SPECIAL_TOKENS
-        self._tokens += extra_tokens
+        self._tokens += list(extra_tokens_mapping.values())
         self._tokens = sorted(list(set(self._tokens)))
         
         self._stoi = {token: i for i, token in enumerate(self._tokens)}
         self._itos = {i: token for token, i in self._stoi.items()}
 
+        self._extra_tokens_mapping = extra_tokens_mapping
+
     def _load_tokens(self, tokens_file: str) -> List:
         with open(tokens_file, "r") as f:
             return json.load(f)
-
-    @abstractmethod
-    def _insert_extra_tokens(self, s: str) -> str:
-        ...
-
-    @abstractmethod
-    def _remove_extra_tokens(self, s: str) -> str:
-        ...
 
     def add_start_stop_tokens(self, s: str) -> str:
         return f"<start> {s} <stop>"
@@ -65,12 +58,24 @@ class SetOfWordsTokenizer(ITokenizer):
 
         return [self._stoi.get(x, self._stoi["<unknown>"]) for x in words]
     
+    def _insert_extra_tokens(self, s: str) -> str:
+        for k, v in self._MAPPING.items():
+            s = s.replace(k, f" {v}")
+    
+        return s
+    
     def itos(self, input: List[int]) -> str:        
         words = [self._itos[x] for x in input]
 
         s = " ".join(words)
         s = self._remove_extra_tokens(s)
 
+        return s
+    
+    def _remove_extra_tokens(self, s: str) -> str:
+        for k, v in self._MAPPING.items():
+            s = s.replace(f" {v}", k)
+            
         return s
     
 
@@ -83,20 +88,9 @@ class SetOfWordsTokenizerRepShort(SetOfWordsTokenizer):
     def __init__(self, dataset_path: str):
         super().__init__(
             tokens_file=os.path.join(dataset_path, "target_tokens.json"),
-            extra_tokens=list(self._MAPPING.values())
+            extra_tokens_mapping=self._MAPPING
         )
-
-    def _insert_extra_tokens(self, s: str) -> str:
-        for k, v in self._MAPPING.items():
-            s = s.replace(k, f" {v}")
     
-        return s
-
-    def _remove_extra_tokens(self, s: str) -> str:
-        for k, v in self._MAPPING.items():
-            s = s.replace(f" {v}", k)
-            
-        return s
 
 class SetOfWordsTokenizerGPT(SetOfWordsTokenizer):
     _MAPPING = {
@@ -104,27 +98,15 @@ class SetOfWordsTokenizerGPT(SetOfWordsTokenizer):
             ",": "<comma>",
             "!": "<exclamationMark>",
             "\"": "<quote>",
-            ":": " <colon>",
-            "?": " <questionMark>",
-            "(": " <openBraket>",
-            ")": " <closeBraket>",
-            ";": " <semicolon>",
+            ":": "<colon>",
+            "?": "<questionMark>",
+            "(": "<openBraket>",
+            ")": "<closeBraket>",
+            ";": "<semicolon>",
         }
     
     def __init__(self, dataset_path: str):
         super().__init__(
             tokens_file=os.path.join(dataset_path, "target_tokens_gpt_filtered.json"), 
-            extra_tokens=list(self._MAPPING.values())
+            extra_tokens_mapping=self._MAPPING
         )
-
-    def _insert_extra_tokens(self, s: str) -> str:
-        for k, v in self._MAPPING.items():
-            s = s.replace(k, f" {v}")
-    
-        return s
-
-    def _remove_extra_tokens(self, s: str) -> str:
-        for k, v in self._MAPPING.items():
-            s = s.replace(f" {v}", k)
-
-        return s
