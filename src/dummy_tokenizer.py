54e6350aa34a3f1c5ef4bfd3da954ff331ead260
Niels Hannemann <92185123+nielshannemann@users.noreply.github.com>
2025-01-18 14:25:56 +0100

    remove old code

--- src/dummy_tokenizer.py
+++ src/dummy_tokenizer.py
@@ -54,49 +54,11 @@ class DummyTokenizer:
         
         words = input.split()
 
-        # if word is not in _stoi_targets, add it to "target_tokens.json"
-        for word in words:
-            if word not in self._stoi_targets:
-                # open "dataset/target_tokens.json"
-                with open(os.path.join(self._dataset_path, "target_tokens.json"), "r") as f:
-                    target_tokens = json.load(f)
-                # add word to target_tokens
-                target_tokens.append(word)
-                # save target_tokens to "dataset/target_tokens.json"
-                with open(os.path.join(self._dataset_path, "target_tokens.json"), "w") as f:
-                    json.dump(target_tokens, f)
-                # update _target_tokens
-                self._target_tokens = target_tokens
-                self._target_tokens += ["<start>", "<stop>", "<punkt>", "<komma>", "<space>", "<padding>"]
-                # update _stoi_targets
-                self._stoi_targets = {token: i for i, token in enumerate(self._target_tokens)}
-                # update _itos_targets
-                self._itos_targets = {i: token for token, i in self._stoi_targets.items()}
-
         return [self._stoi_targets[x] for x in words]
     
     def stoi_context(self, input: str) -> List[int]:        
         words = input.split(",")
-
-        # if word is not in _stoi_context, add it to "context_tokens.json"
-        for word in words:
-            if word not in self._stoi_context:
-                # open "dataset/context_tokens.json"
-                with open(os.path.join(self._dataset_path, "context_tokens.json"), "r") as f:
-                    context_tokens = json.load(f)
-                # add word to context_tokens
-                context_tokens.append(word)
-                # save context_tokens to "dataset/context_tokens.json"
-                with open(os.path.join(self._dataset_path, "context_tokens.json"), "w") as f:
-                    json.dump(context_tokens, f)
-                # update _context_tokens
-                self._context_tokens = context_tokens
-                self._context_tokens += ["<padding>"]
-                # update _stoi_context
-                self._stoi_context = {token: i for i, token in enumerate(self._context_tokens)}
-                # update _itos_context
-                self._itos_context = {i: token for token, i in self._stoi_context.items()}
-
+        
         return [self._stoi_context[x] for x in words]
 
     def itos_targets(self, input: List[int]) -> str:        
