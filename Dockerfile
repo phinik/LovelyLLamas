68c522969b709ce03ec4963a2ffbcd5348f5f708
Franko Jolic <@>
2025-01-29 03:30:38 +0100

    Demo Done and Working

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM python:3.11-slim
 WORKDIR /app
-COPY requirements.txt .
+
+COPY demo/backend/requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-COPY . .
+
+COPY src /app/src
+COPY demo/backend /app/
+
 CMD ["python", "app.py"]
--- demo/backend/app.py
+++ demo/backend/app.py
@@ -1,23 +1,58 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from generate import *
 import json
+import os
 app = Flask(__name__)
 CORS(app)
 
-with open('files.json', 'r') as f:
-    files = json.load(f)
-    # concatenate city with id
-    CITIES = [f"{value['city']} ({value['id']})" for key, value in files.items()]
+# Get List of cities
+with open(os.path.join(os.getcwd(), 'assets', 'cities.json'), 'r', encoding = 'utf-8') as f:
+    data = json.load(f)
+    CITIES = []
+    for id, vals in data.items():
+        CITIES.append(f'{vals[0]} ({id})')
+    # sort cities alphabetically
+    CITIES.sort()
+
+with open(os.path.join(os.getcwd(), 'data', 'dset_test.json'), 'r', encoding = 'utf-8') as f:
+    dset: list = json.load(f)
+
+config = {
+    "name": "Whatever",
+    "dataset": os.path.join(os.getcwd(), 'data'),
+    "model_weights": os.path.join(os.getcwd(), "src_model", "best_model_CE_loss.pth"),
+    "model_params": os.path.join(os.getcwd(), "src_model", "params.json"),
+    "cached": True,
+    "model": "transformer", #args.model,
+    "block_size": 20,
+    "tokenizer": 'bert',
+    "target": "default"
+    }
+
+# Load model
+model = TransformerFactory.from_file(config["model_params"])
+model.load_weights_from(config["model_weights"])
+model.to(DEVICE)
+generator = Generator(config)
+
 
 @app.route('/api/mock-llm', methods=['POST'])
 def mock_llm_response():
     city = request.json.get('city', '')
+    id = str(city).split('(')[-1][:-1]
+    file_name = data[id][1]
+    print(file_name)
+    idx = dset.index(f'dataset/{file_name}')
+    print(idx)
+    text = generator.get(model, int(idx))
     return jsonify({
-        'response': f"Based on my analysis, {city} is a fascinating city with a rich cultural heritage. "
-                   f"The city offers numerous attractions, diverse cuisine, and unique architectural styles. "
-                   f"Visitors to {city} often praise its blend of historical landmarks and modern amenities. "
-                   f"The local transportation system is well-developed, making it easy to explore various neighborhoods. "
-                   f"Would you like to know more about any specific aspect of {city}?"
+        # 'response': f"Based on my analysis, {city} is a fascinating city with a rich cultural heritage. "
+        #            f"The city offers numerous attractions, diverse cuisine, and unique architectural styles. "
+        #            f"Visitors to {city} often praise its blend of historical landmarks and modern amenities. "
+        #            f"The local transportation system is well-developed, making it easy to explore various neighborhoods. "
+        #            f"Would you like to know more about any specific aspect of {city}?"
+        'response': text
     })
 
 @app.route('/api/cities', methods=['GET'])
